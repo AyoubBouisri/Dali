e96e6ddac7df188de571629c44aaea1cc0355a15
ayoubbouisri <ayoubbouisri@hotmail.fr>
2021-10-16 01:48:58 -0400

    If you blunder he taketh

--- game/bot.py
+++ game/bot.py
@@ -8,20 +8,20 @@ class Dali():
     def __init__(self):
         pass
 
-    def eval(board):
-        pass 
-
+    def eval_move(self, board, move):
+        board.push(move)
+        eval = board_eval(board)
+        board.pop()
+        return eval
 
     def play(self, board):
         legal_moves = list(board.legal_moves)
-        best_move = (legal_moves[0], 0)
+        best_move = (legal_moves[randrange(len(legal_moves))], 0)
         
         for i in range(0, len(legal_moves)):
             move = legal_moves[i]
-            board.push(move)
-            eval = board_eval(board)
+            eval = self.eval_move(board, move)
             if eval < best_move[1]:
                 best_move = (move, eval)
-            board.pop()
                 
         board.push(best_move[0])
