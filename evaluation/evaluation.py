92ece931d7aaf43e08dd202a88d25d6b3b55636c
ayoubbouisri <ayoubbouisri@hotmail.fr>
2021-10-21 22:10:29 -0400

    alpha beta pruning

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -1,5 +1,6 @@
 import chess
 import time
+import math
 
 PIECES_WORTH = [1, 3, 3, 5, 9, 0]
 TIME_LIMIT = 10000 # ms
@@ -19,7 +20,8 @@ class Evaluator():
         self.max_depth_reached = 0
         self.nbr_moves_searched = 0
 
-        best_move = self.search(board, isMax, 0)
+        #best_move = self.search_min_max(board, isMax, 0)
+        best_move = self.search_alpha_beta(board, isMax, 0, -math.inf, math.inf)
 
         print(f"Depth Reached : {self.max_depth_reached}")
         print(f"Time Searching : {self.get_search_time()}")
@@ -27,8 +29,40 @@ class Evaluator():
 
         return best_move[1]
 
-    def search(self, board, isMax, depth):
+    def search_alpha_beta(self, board, isMax, depth, alpha, beta):
 
+        self.max_depth_reached = depth if depth > self.max_depth_reached else self.max_depth_reached
+        self.nbr_moves_searched += 1
+
+        if not board.legal_moves or depth >= MAX_DEPTH:
+            return self.eval_position(board), None
+        
+        if isMax:
+            max_value = (-math.inf, None)
+            for move in self.order_legal_moves(board):
+                board.push(move)
+                best_move = self.search_alpha_beta(board, False, depth + 1, alpha, beta)
+                board.pop()
+
+                max_value = max([max_value, (best_move[0], move)], key=lambda t:t[0])
+                alpha = max(alpha, best_move[0]) 
+                if beta <= alpha:
+                    break
+            return max_value
+        else:
+            min_value = (math.inf, None)
+            for move in self.order_legal_moves(board):
+                board.push(move)
+                best_move = self.search_alpha_beta(board, True, depth + 1, alpha, beta)
+                board.pop()
+
+                min_value = min([min_value, (best_move[0], move)], key=lambda t: t[0])
+                beta = min(best_move[0], beta)
+                if beta <= alpha:
+                    break
+            return min_value
+                
+    def search_min_max(self, board, isMax, depth):
         self.max_depth_reached = depth if depth > self.max_depth_reached else self.max_depth_reached
         self.nbr_moves_searched += 1
 
@@ -38,7 +72,7 @@ class Evaluator():
         moves = []
         for move in board.legal_moves:
             board.push(move)
-            best_move = self.search(board, not isMax, depth + 1)
+            best_move = self.search_min_max(board, not isMax, depth + 1)
             moves.append((best_move[0], move))
             board.pop()
 
@@ -50,11 +84,12 @@ class Evaluator():
         else:
             return min(moves, key=lambda t: t[0])
 
-    def order_moves(self, board, moves: list):
+    def order_legal_moves(self, board):
         checks = []
         piece_attacks = []
         others = []
         
+        moves = board.legal_moves
         for move in moves:
             board.push(move)
             if board.is_check():
@@ -63,12 +98,16 @@ class Evaluator():
                 is_attacking = False
                 squares_attacked = board.attacks(move.to_square)
                 for square in squares_attacked:
-                    if board.piece_at(square).color is board.turn:
+                    piece = board.piece_at(square) 
+                    if piece and piece.color == board.turn:
                         piece_attacks.append(move)
                         is_attacking = True
                         break;
+
                 if not is_attacking:
                     others.append(move)
+            board.pop()
+
         return checks + piece_attacks + others
 
     def eval_position(self, board):
